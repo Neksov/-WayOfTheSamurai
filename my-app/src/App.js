68aa6a3ea98989026bba1119da03919b2e105edd
Nekrasov Vadim <n2v@list.ru>
2021-06-15 23:01:54 +0300

    add new component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,34 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import Tehno from './Tehno';
+import './Tehno'
 
 const App = () => {
   return (
-    <div className="App">
-        Simple HTML
+    <div>
         <Header/>
         <Tehno/>
     </div>
   );
 };
 
-const Header = () => {
-  return (
-    <div>
-      <a href="#">Home</a>
-      <a href="#">Contact</a>
-      <a href="#">About</a>
-    </div>
-  );
-}
-const Tehno = ()=>{
-  return(
-    <ul>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-      <li>4</li>
-      <li>5</li>
-    </ul>
-  );
-}
 export default App;
---
+++ my-app/src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      <a href="#">Home</a>
+      <a href="#">Contact</a>
+      <a href="#">About</a>
+    </div>
+  );
+}
+export default Header;
